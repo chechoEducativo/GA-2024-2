128f7628c591fe5d380da48b3df57866ef7f13b8
chechoEducativo <edu.sergio.penaloa@gmail.com>
2024-08-06 11:12:12 -0500

    Character movement in class

--- Assets/Sessions/4 Inputs+RootMotion/Class/CharacterMovement.cs
+++ Assets/Sessions/4 Inputs+RootMotion/Class/CharacterMovement.cs
@@ -4,14 +4,23 @@ using CallbackContext = UnityEngine.InputSystem.InputAction.CallbackContext;
 public class CharacterMovement : MonoBehaviour
 { 
     [SerializeField] private Animator anim;
-    [SerializeField] private VectorDampener motionVector;
+    [SerializeField] private VectorDampener motionVector = new VectorDampener(true);
+    
     private int velXId;
     private int velYId;
+    
     public void Move(CallbackContext ctx)
     {
         Vector2 direction = ctx.ReadValue<Vector2>();
         motionVector.TargetValue = direction;
     }
+
+    public void ToggleSprint(CallbackContext ctx)
+    {
+        bool val = ctx.ReadValueAsButton();
+        motionVector.Clamp = !val;
+    }
+    
     private void Awake()
     {
         velXId = Animator.StringToHash("VelX");
